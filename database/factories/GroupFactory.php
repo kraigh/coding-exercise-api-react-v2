a0f16ef04bbe02fbcb10a7fea51e988854b1ff27
Kraig Hufstedler <kraigory@gmail.com>
2020-07-24 14:58:11 -0500

    added groups model, controller, and api test

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Group;
+use Faker\Generator as Faker;
+
+$factory->define(Group::class, function (Faker $faker) {
+    return [
+        'group_name' => $faker->randomElement($array = array ('Elders','Volunteers','Staff','Members','Visitors'))
+    ];
+    
+});
